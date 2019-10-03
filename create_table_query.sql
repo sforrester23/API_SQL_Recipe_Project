15bd433da2decafbccc3a29218fa9ed7f8dc1790
Sam Forrester <SForrester@spartaglobal.com>
2019-10-03 23:42:00 +0100

    added SQL file used to make database and table

---
+++ create_table_query.sql
@@ -0,0 +1,32 @@
+--CREATE DATABASE Recipe_Book
+--USE Recipe_Book;
+
+--CREATE TABLE Recipe_List
+--(
+--Recipe_Name VARCHAR(25),
+--Ingredients VARCHAR(500),
+--Method VARCHAR(1000),
+--Postcode VARCHAR(10)
+--)
+
+--INSERT INTO Recipe_List
+--VALUES 
+--('Chicken Fajitas', '3 x Chicken breast, 1 x Large Onion, 3 x Mixed Peppers, 1tsp Paprika, 1tsp Cumin, 1/2tsp Cayenne Pepper, Salt & Pepper, 8 x Flour Tortillas, Salsa, Grated Cheddar, Sour Cream, Mixed Salad, Olive Oil', '1) Chop the chicken, onions and peppers into small chunks. 2) Heat the oil in a pan, with the spices and stir to make a paste. 3) Add the chicken and the onions to coat in the spices, cook until chicken is brown and onions are soft. 4) Add the peppers to the mix and cook for a further 5-10 mins, until the peppers are soft but not too soft. 5) Serve with Tortillas, cheese, sour cream, salsa and salad.', 'LS1 2SJ')
+
+--INSERT INTO Recipe_List
+--VALUES 
+--('Toad in the Hole', '6 x Pork Sausages, 2 x Eggs, Milk, 100g Plain Flour, Salt & Pepper, Olive Oil', '1) Pre-heat the oven, once warm place the sausages in the olive oil and cook for 10 mins. 2) While they are cooking, make the batter: place the flour in a bowl, add the eggs and then incrementally add the milk and stir/whisk until it becomes a runny batter. 3) Pour the batter over the sausages and cook for 30-40 mins, until the batter is risen and crispy. 4) Serve with vegetables and gravy.', 'BL3 5LJ')
+
+--INSERT INTO Recipe_List
+--VALUES 
+--('Corn Flakes', 'Corn Flakes, Milk, dash of sugar', '1) Pour cereal into bowl. 2) Add milk. 3) sprinkle some sugar on. 4) Bone app the teeth. ', 'CT20 3FU')
+
+--INSERT INTO Recipe_List
+--VALUES 
+--('Kentucky Fried Chicken', 'Time, Money', '1) Go to KFC store. 2) Purchase Chicken. 3) Enjoy Chicken. 4) Cry.', 'M8 4JB')
+
+--INSERT INTO Recipe_List
+--VALUES
+--('Pizza', 'Dough, Tomato Sauce, Mozarella, Toppings', '1) Flatten the dough and spread the tomato sauce on it. 2) Add the cheese and spread it evenly. 4) Add the toppings to your liking, and top up with cheese on top. 5) Bake for 10-15 minutes until the cheese is golden brown.', 'TR16 6PS')
+
+SELECT * FROM Recipe_List
